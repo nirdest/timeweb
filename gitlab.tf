50399aa7915102f4f0d18cc36bde26a846303928
Denis Kuzmin <nirdest@gmail.com>
2023-07-09 04:37:53 +0600

    Terraform manifest for create server

---
+++ gitlab.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    twc = {
+      source = "tf.timeweb.cloud/timeweb-cloud/timeweb-cloud"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "twc" {
+  token = "${{ secrets.TIMEWEB_TOKEN }}"
+}
+
+data "twc_configurator" "configurator" {
+  location = "kz-1"
+}
+
+data "twc_os" "os" {
+  name    = "ubuntu"
+  version = "22.04"
+}
+
+data "twc_ssh_keys" "ssh-key" {
+  name = "macbook"
+}
+
+data "twc_projects" "project" {
+  name = "terraform"
+}
+
+resource "twc_server" "my-timeweb-server" {
+  name         = "GitLab"
+  os_id        = data.twc_os.os.id
+  ssh_keys_ids = [data.twc_ssh_keys.ssh-key.id]
+  project_id   = data.twc_projects.project.id
+
+  configuration {
+    configurator_id = data.twc_configurator.configurator.id
+    disk            = 1024 * 50
+    cpu             = 2
+    ram             = 1024 * 4
+  }
+}
